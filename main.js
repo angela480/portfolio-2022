41350abce57ee940d6ee454754943e139f588c41
angela480 <97154569+angela480@users.noreply.github.com>
2022-02-27 22:57:43 +0900

    edit

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ navbarMenu.addEventListener('click', (event) => {
     const scrollTo = document.querySelector(link);
     scrollTo.scrollIntoView({behavior: "smooth"});
 });
+
+//projects
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_project');
+const projects =  document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter;
+})
