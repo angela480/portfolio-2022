58da516552a88a5b0061857e0f9cf9ca5fc947c4
angela480 <97154569+angela480@users.noreply.github.com>
2022-02-27 15:15:43 +0900

    javascipt

---
+++ main.js
@@ -0,0 +1,13 @@
+// Handle Scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
